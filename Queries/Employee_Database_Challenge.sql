d3c92569c35ca523d40334ce8bac68ad33f1331c
apaneser <56094764+apaneser@users.noreply.github.com>
2021-08-08 23:05:46 -0700

    deliverable 2

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -17,3 +17,15 @@ INTO retireing_titles
 FROM unique_titles as ut
 GROUP BY title
 ORDER BY Count(title) DESC;
+
+SELECT DISTINCT ON (emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, 
+	de.from_date, de.to_date, t.title
+INTO mentorship_eligibility
+FROM employees as e
+LEFT JOIN title as t
+ON e.emp_no = t.emp_no
+LEFT JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+WHERE (de.to_date = '9999-01-01') 
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no ASC
